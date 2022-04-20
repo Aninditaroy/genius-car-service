1774cb1387a5b38fb459ec29bf75c755e4a5ad8f
aninditaroy <anindita.roy.mu@gmail.com>
2022-04-20 10:36:11 +0600

    Set Dynamic page title based on Route using react helmet async

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import PageTitle from '../Shared/PageTitle/PageTitle';
 import Banner from './Banner/Banner';
 import Experts from './Experts/Experts';
 import Services from './Services/Services';
@@ -6,9 +8,10 @@ import Services from './Services/Services';
 const Home = () => {
     return (
         <>
+            <PageTitle title="Home"/>
             <Banner />
             <Services />
-            <Experts /> 
+            <Experts />
         </>
     );
 };
--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -1,16 +1,34 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
-const RequireAuth = ({children}) => {
+const RequireAuth = ({ children }) => {
     const location = useLocation()
-    const [user,loading] = useAuthState(auth);
-    if(loading){
-        return <Loading/>
+    const [user, loading] = useAuthState(auth);
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
+    if (loading) {
+        return <Loading />
     }
-    if(!user){
-        return <Navigate to="/login" state={{from: location}} replace/>
+    if (!user) {
+        return <Navigate to="/login" state={{ from: location }} replace />
+    }
+    if (!user.emailVerified) {
+        return <div className='text-center mt-5'>
+            <h3 className='text-danger'>Your email is not verified</h3>
+            <h5 className='text-success'>Please verify your email address</h5>
+            <button className='btn btn-primary'
+                onClick={async () => {
+                    await sendEmailVerification();
+                    toast('Sent email');
+                }}
+            >
+              Send Verification Email Again
+            </button>
+            <ToastContainer></ToastContainer>
+        </div>
     }
     return children;
 };
